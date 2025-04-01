f2cede69d7483bcd4af666e0387a8e8010366ab4
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2025-04-01 19:01:07 +0300

    Son_Guncelleme

---
+++ TeknikService_Web/TeknikService_Web/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TeknikService_Web.Entity;
+namespace TeknikService_Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBL_HAKKIMIZDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = db.TBL_URUN.ToList();
+            Repeater2.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TBL_ILETISIM t = new TBL_ILETISIM();
+            t.ADSOYAD = TextBox1.Text;
+            t.MAIL = TextBox2.Text;
+            t.KONU = TextBox3.Text;
+            t.MESAJ = TextBox4.Text;
+            db.TBL_ILETISIM.Add(t);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ TeknikService_Web/TeknikService_Web/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TeknikService_Web.Entity;
+namespace TeknikService_Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DbTeknikServisEntities db = new DbTeknikServisEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var degerler = db.TBL_URUNTAKIP.Where(x => x.SERINO == TextBox1.Text);
+            Repeater1.DataSource = degerler.ToList();
+            Repeater1.DataBind();
+        }
+    }
+}
